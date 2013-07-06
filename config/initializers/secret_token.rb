7c69ad80b7bf8dfdb820b96d9ad74e6156b6c286
Lucas Boyer <lucasjb92@gmail.com>
2013-07-06 13:55:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3app::Application.config.secret_token = '36c9e84067fcf61f142f709160bd608b03e6e34da3cdf62d9de62984481ee4213ede2ddc473bab1aa6558c011bee3c9685239c1a65aac42305666730bcbe048c'
