a7accec745f08b7c7d81c71901901f50e292d9bb
Lucas Boyer <lucasjb92@gmail.com>
2013-07-06 17:50:14 -0400

    Finished static pages

---
+++ spec/requests/statics_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Statics" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home/'
+      page.should have_content('Sample App')
+    end
+    it "should have the right title" do
+        visit '/static_pages/home'
+        page.should have_selector('title', :text => "RoR Sample | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "RoR Sample | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "RoR Sample | About")
+    end
+  end
+
+end
